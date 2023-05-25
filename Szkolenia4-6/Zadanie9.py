d711c7aea8e30d74b956ab3ebcb985a17d7a165e
github-example-dvm <cryptovadkan@gmail.com>
2023-05-25 23:43:24 +0200

    logika

---
+++ Szkolenia4-6/Zadanie9.py
@@ -0,0 +1,26 @@
+# true and false
+
+a = True
+b = False
+
+(a and b) == False
+(a or b) == True
+
+c = True
+d = True
+
+(c and d) == True
+(c or d) == True
+
+e = False
+f = False
+
+(e and f) == False
+(e or f) == False
+
+g = False
+h = True
+
+(g and h) == False
+(g or h) == True
+
